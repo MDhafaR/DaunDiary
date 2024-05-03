fbbbe9d591bdca978e1c0e43071d191ec40a51f7
Muhammad Dhafa Ramadhani <dhafaramadhani2004@gmail.com>
2024-05-03 22:02:56 +0700

    tampilan awal pengguna

--- app/src/main/java/org/d3if3068/assesment2/daundiary/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3068/assesment2/daundiary/ui/screen/MainScreen.kt
@@ -1,18 +1,46 @@
 package org.d3if3068.assesment2.daundiary.ui.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.d3if3068.assesment2.daundiary.R
 import org.d3if3068.assesment2.daundiary.ui.theme.DaunDiaryTheme
+import org.d3if3068.assesment2.daundiary.ui.theme.LightPrimary
+import org.d3if3068.assesment2.daundiary.ui.theme.Purple80
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -21,13 +49,28 @@ fun MainScreen() {
         topBar = {
             TopAppBar(
                 title = {
-                    Text(text = stringResource(id = R.string.app_name))
+                    Image(
+                        modifier = Modifier.size(171.dp),
+                        painter = painterResource(id = R.drawable.logo),
+                        contentDescription = stringResource(
+                            id = R.string.app_name
+                        )
+                    )
                 },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    containerColor = LightPrimary,
                     titleContentColor = MaterialTheme.colorScheme.primary
                 )
             )
+        },
+        floatingActionButton = {
+            IconButton(onClick = { /*TODO*/ }, Modifier.size(60.dp)) {
+                Image(
+                    painter = painterResource(id = R.drawable.fab),
+                    contentDescription = "fab",
+                    Modifier.size(50.dp)
+                )
+            }
         }
     ) { padding ->
         ScreenContent(Modifier.padding(padding))
@@ -36,7 +79,56 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier) {
+    Box(
+        modifier = modifier
+    ) {
+        Image(
+            modifier = Modifier
+                .size(400.dp)
+                .offset(y = -75.dp),
+            painter = painterResource(id = R.drawable.lightback),
+            contentDescription = "Pagi"
+        )
+        LazyColumn {
+            item {
+                Column(
+                    modifier = Modifier
+                        .height(200.dp)
+                ) {}
+            }
 
+            item {
+                Column(
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(topStart = 17.dp, topEnd = 17.dp))
+                        .background(Color.White)
+                        .height(660.dp)
+                        .fillMaxWidth()
+                ) {
+                    // content kosong
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier
+                            .fillMaxSize()
+                    ) {
+                        Image(
+                            modifier = Modifier
+                                .size(163.dp),
+                            painter = painterResource(id = R.drawable.emptyimage),
+                            contentDescription = "Empty Data"
+                        )
+                        Text(
+                            fontSize = 36.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = LightPrimary,
+                            text = "Buat DiaryMu",
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
